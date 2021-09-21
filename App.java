c4f88548982889902ae4265d4df789b72ceaa69b
DiegoCoss <91162707+DiegoCoss@users.noreply.github.com>
2021-09-21 18:07:23 -0500

    Add files via upload

---
+++ App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Buenos dias");
+        //Diego
+        /*
+        Shrek best movie
+        */
+        /**
+         * Gitgut
+         * ez
+         * gn
+         * dog water
+         */
+    }
+}
